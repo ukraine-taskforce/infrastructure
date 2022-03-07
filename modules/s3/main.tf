525f9d3025a896d735b3795f535298f3f10d4bc2
Paul Williams <paul@skenmy.com>
2022-03-07 00:24:08 +0000

    Add S3 Policy, CloudFlare record

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -60,9 +60,63 @@ module "s3_bucket" {
 
   bucket = local.env_domain_name
   acl    = "public-read"
+  attach_policy = true
+  policy = <<POLICY
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "PublicReadGetObject",
+            "Effect": "Allow",
+            "Principal": "*",
+            "Action": "s3:GetObject",
+            "Resource": "arn:aws:s3:::${local.domain_name}/*",
+            "Condition": {
+                "IpAddress": {
+                    "aws:SourceIp": [
+                        "2400:cb00::/32",
+                        "2606:4700::/32",
+                        "2803:f800::/32",
+                        "2405:b500::/32",
+                        "2405:8100::/32",
+                        "2a06:98c0::/29",
+                        "2c0f:f248::/32",
+                        "173.245.48.0/20",
+                        "103.21.244.0/22",
+                        "103.22.200.0/22",
+                        "103.31.4.0/22",
+                        "141.101.64.0/18",
+                        "108.162.192.0/18",
+                        "190.93.240.0/20",
+                        "188.114.96.0/20",
+                        "197.234.240.0/22",
+                        "198.41.128.0/17",
+                        "162.158.0.0/15",
+                        "172.64.0.0/13",
+                        "131.0.72.0/22",
+                        "104.16.0.0/13",
+                        "104.24.0.0/14"
+                    ]
+                }
+            }
+        }
+    ]
+}
+POLICY
 
   website = {
     index_document = "index.html"
   }
 
+}
+
+resource "cloudflare_record" "frontend" {
+  zone_id = data.cloudflare_zone.this.id
+  name    = var.production ? "@" : "dev"
+  type    = "CNAME"
+  value   = module.s3.s3_bucket_website_endpoint
+  ttl     = 1
+  proxied = true
+
+  allow_overwrite = true
 }
