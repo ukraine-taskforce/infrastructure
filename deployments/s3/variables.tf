9bde70782dd72474e894bfa515a21d83dd932e09
Rodrigo Oliveira <rodrigo.oliveira@on-running.com>
2022-03-08 21:11:28 +0100

    Default region

--- deployments/s3/variables.tf
+++ deployments/s3/variables.tf
@@ -1,6 +1,7 @@
 variable "region" {
   description = "Region"
   type = string
+  default = "eu-central-1"
 }
 
 variable "env_name" {
@@ -23,25 +24,25 @@ variable "fe_subdomain" {
   type = string
 }
 
-variable "lambda_locations_key" { 
+variable "lambda_locations_key" {
   description = "S3 Object key for Locations lambda"
   type = string
   default = "locations.zip"
 }
 
-variable "lambda_supplies_key" { 
+variable "lambda_supplies_key" {
   description = "S3 Object key for Supplies lambda"
   type = string
   default = "supplies.zip"
 }
 
-variable "lambda_requests_key" { 
+variable "lambda_requests_key" {
   description = "S3 Object key for Requests lambda"
   type = string
   default = "requests.zip"
 }
 
-variable "lambda_processor_key" { 
+variable "lambda_processor_key" {
   description = "S3 Object key for Processor lambda"
   type = string
   default = "processor.zip"
