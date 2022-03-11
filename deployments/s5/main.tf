f6f5d99af72d8115e51a2cbb7386bb98dae805e6
Theo Dammaretz <theo@dammaretz.fr>
2022-03-11 15:12:31 +0100

    feat: init infrastructure for S5

---
+++ deployments/s5/main.tf
@@ -0,0 +1,8 @@
+module "s3" {
+  source = "../../modules/s3"
+
+  region = var.region
+  env_name = var.env_name
+  domain_name = var.domain_name
+}
+
---
+++ modules/s5/outputs.tf
@@ -0,0 +1,4 @@
+output "vpc_id" { 
+    description = "The VPC ID"
+    value = module.vpc.vpc_id
+}
