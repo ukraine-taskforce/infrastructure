6db00d3a1e1ebb1d50972ed37301894533a91c69
Theo Dammaretz <theo@dammaretz.fr>
2022-03-12 12:51:07 +0100

    chore: tweaks vpxc

--- modules/s1/main.tf
+++ modules/s1/main.tf
@@ -15,12 +15,10 @@ module "vpc" {
   name = join("-", [var.env_name, var.region, "vpc"])
   cidr = "10.0.0.0/16"
 
-  azs             = ["eu-central-1a", "eu-central-1b", "eu-central-1c"]
+  azs             = [join([var.region, "a"]), join([var.region, "b"]), join([var.region, "c"])]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
-  enable_nat_gateway     = true
-  single_nat_gateway     = true
   one_nat_gateway_per_az = false
 }
 
