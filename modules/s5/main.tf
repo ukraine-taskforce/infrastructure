1baa92ed88bba09cd884ee7c375cc8e004205c9d
Theo Dammaretz <theo@dammaretz.fr>
2022-03-16 13:50:08 +0100

    feat: add cognito

--- modules/s5/main.tf
+++ modules/s5/main.tf
@@ -114,3 +114,22 @@ resource "cloudflare_record" "frontend" {
 
   allow_overwrite = true
 }
+
+### Authentication
+resource "aws_cognito_user_pool" "users" {
+  name = join("-", [var.env_name, var.region, "pool"])
+
+  
+  mfa_configuration = "OFF"
+
+  account_recovery_setting {
+    recovery_mechanism {
+      name     = "verified_email"
+      priority = 1
+    }
+  }
+
+  admin_create_user_config {
+    allow_admin_create_user_only = true
+  }
+}
