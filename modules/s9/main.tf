9de8619b52c99b0849492359ba928d03f019976a
Apan Trikha <apan007@live.co.uk>
2022-04-24 18:38:00 +0530

    Adding S9 Lambda for CSV to DyanmoDB

--- modules/s9/main.tf
+++ modules/s9/main.tf
@@ -108,3 +108,51 @@ resource "aws_s3_bucket" "ugt_lambda_states" {
 
   force_destroy = true
 }
+
+### IAM Roles
+resource "aws_iam_role" "supply_data_role" {
+  name = "supply_data_role"
+
+  assume_role_policy = <<EOF
+    {
+      "Version": "2012-10-17",
+      "Statement": [
+        {
+          "Action": "sts:AssumeRole",
+          "Principal": {
+            "Service": "lambda.amazonaws.com"
+          },
+          "Effect": "Allow",
+          "Sid": ""
+        }
+      ]
+    }
+  EOF
+}
+
+### Backend Lambdas
+resource "aws_lambda_function" "supply_data" {
+  
+  function_name = "SupplyData"
+  description = "Converts CSV data and supplies it to DynamoDB"
+
+  s3_bucket = aws_s3_bucket.ugt_lambda_states.id
+  s3_key    = var.lambda_supply_data_key
+
+  handler = "main.lambda_handler"
+  runtime = "python3.9"
+
+  role = aws_iam_role.supply_data_role.arn
+}
+
+### Backend DynamoDB
+resource "aws_dynamodb_table" "sos-info" {
+  name = join("-", [var.env_name, var.region, "sos-info"])
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key = "ID"
+
+  attribute {
+    name = "ID"
+    type = "S"
+  }
+}
