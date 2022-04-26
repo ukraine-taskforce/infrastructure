6386b455c03fb6edf001d8ab04aba0dae4693906
Fred <879282+Minimoi@users.noreply.github.com>
2022-04-26 22:11:58 +0200

    Remove policy leading spaces

--- modules/s9/backend.tf
+++ modules/s9/backend.tf
@@ -18,20 +18,19 @@ resource "aws_iam_role" "supply_data_role" {
   name = "supply_data_role"
 
   assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
     {
-      "Version": "2012-10-17",
-      "Statement": [
-        {
-          "Action": "sts:AssumeRole",
-          "Principal": {
-            "Service": "lambda.amazonaws.com"
-          },
-          "Effect": "Allow",
-          "Sid": ""
-        }
-      ]
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow"
     }
-  EOF
+  ]
+}
+EOF
 }
 
 resource "aws_iam_role_policy_attachment" "supply_data_lambda_policy_attachment" {
