863ebcbe1f02fc8d52db5245fbc3ab3017bc937e
Fred <879282+Minimoi@users.noreply.github.com>
2022-04-26 22:21:24 +0200

    Typo S3 trigger

--- modules/s9/backend.tf
+++ modules/s9/backend.tf
@@ -82,7 +82,7 @@ resource "aws_s3_bucket_notification" "supply_data_notification" {
   lambda_function {
     lambda_function_arn = aws_lambda_function.supply_data.arn
     events              = ["s3:ObjectCreated:*"]
-    filter_prefix       = "/assets/dataset"
+    filter_prefix       = "assets/dataset"
     filter_suffix       = ".csv"
   }
 
